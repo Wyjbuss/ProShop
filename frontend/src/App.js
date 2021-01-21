10a241d379a6c06679740d7ee73313ba63643470
Wyjbuss <wyjbuss@gmail.com>
2021-01-21 16:20:54 -0600

    Added Starting files, cleaned up, created header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+ <div>
+ <Header />
+  <h1>Welcome to ProShop</h1>
+  <Footer />
+ </div>
+  );
+}
+
+export default App;
