4ccd7c18b6f45e9a2bb4a04ff4a1eb0e33579ba9
Wyjbuss <wyjbuss@gmail.com>
2021-01-21 18:46:39 -0600

    Navbar in header, font awesome cdn lnk tag in index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 
+
 function App() {
   return (
- <div>
+ <>
  <Header />
-  <h1>Welcome to ProShop</h1>
+ <main className="py-3">
+ <Container>
+ <h1>Welcome to ProShop</h1>
+ </Container>
+ </main>
   <Footer />
- </div>
+ </>
   );
 }
 
